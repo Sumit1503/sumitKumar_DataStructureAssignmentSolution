5dd73a58021972133dd12816284468c19edb9bc2
Sumit_Kumar <sumit15392@gmail.com>
2022-05-06 13:16:06 +0530

    Committing the models for both question

---
+++ src/in/datastructure/assignment/models/Floor.java
@@ -0,0 +1,8 @@
+package in.datastructure.assignment.models;
+
+public class Floor {
+	
+	// model of a floor
+	public int size;
+	public int day;
+}
---
+++ src/in/datastructure/assignment/models/Node.java
@@ -0,0 +1,13 @@
+package in.datastructure.assignment.models;
+
+public class Node {
+	
+	public int value;
+	public Node left, right;
+
+	public Node(int value) {
+		this.value = value;
+		left = null;
+		right = null;
+	}
+}
